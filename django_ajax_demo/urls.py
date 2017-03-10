c14bde1ce9d15b0981ab496e3756513c0ede1c8e
Ding Li <dingli@Xiaolus-MacBook-Pro.local>
2017-03-10 10:32:41 -0600

    add color_liker/urls.py

--- django_ajax_demo/urls.py
+++ django_ajax_demo/urls.py
@@ -6,5 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'django_ajax_demo.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^color_liker/', include('color_liker.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
 )
